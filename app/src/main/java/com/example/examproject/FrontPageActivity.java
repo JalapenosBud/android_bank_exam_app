4b474d771eecc78d334a6d8dfb1bfe8cc738538d
JalapenosBud <Jalapenosbud@gmail.com>
2019-05-12 23:53:43 +0200

    hih

---
+++ app/src/main/java/com/example/examproject/FrontPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.examproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+}
