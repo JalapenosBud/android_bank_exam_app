dbe12099e5745120fc0613a12ee88879e8fb8b20
JalapenosBud <Jalapenosbud@gmail.com>
2019-05-14 18:20:07 +0200

    heheh

--- app/src/main/java/com/example/examproject/FrontPageActivity.java
+++ app/src/main/java/com/example/examproject/FrontPageActivity.java
@@ -1,7 +1,11 @@
 package com.example.examproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FrontPageActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class FrontPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
+
+        Button sign_in = (Button) findViewById(R.id.button_sign_in);
+
+        Intent sign_in_intent = new Intent(this, HomeActivity.class);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startactivity starts a new activity
+                startActivity(sign_in_intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/examproject/HomeActivity.java
+++ app/src/main/java/com/example/examproject/HomeActivity.java
@@ -1,13 +1,85 @@
 package com.example.examproject;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements TransferFragment.OnFragmentInteractionListener {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    TransferFragment myfrag = new TransferFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn_home = (Button)findViewById(R.id.button_home);
+
+        ImageButton btn_transactions = (ImageButton)findViewById(R.id.button_transactions);
+
+        View fragment_layout = (View) findViewById(R.id.fragment_layout);
+        View sv_main = (View)findViewById(R.id.sv_main);
+
+        //fragment_layout.setVisibility(View.INVISIBLE);
+
+        fragmentManager = getSupportFragmentManager();
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().popBackStack();
+                fragmentTransaction.addToBackStack(null);
+                fragment_layout.setVisibility(View.INVISIBLE);
+
+                sv_main.setVisibility(View.VISIBLE);
+                //only use commit when leaving the activity
+                //fragmentTransaction.commit();
+            }
+        });
+
+        btn_transactions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = fragmentManager.beginTransaction();
+                Log.d("asd","hi");
+                sv_main.setVisibility(View.INVISIBLE);
+
+                if(getSupportFragmentManager().findFragmentByTag(myfrag.getClass().getName()) != null)
+                {
+                    fragmentTransaction.remove(myfrag);
+                }
+                else
+                {
+                    TransferFragment myfrag = new TransferFragment();
+                    fragmentTransaction.add(R.id.fragment_layout, myfrag, TransferFragment.class.getName());
+                }
+
+                fragment_layout.setVisibility(View.VISIBLE);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d("hi","asd");
     }
 }
